d50550829e07d8a229e8f680a6d729c068e3085f
jeferson nascimento <jeffersom732@gmail.com>
2025-11-09 12:55:48 -0300

    Correcting the formatting of display and conversation values.

--- script.js
+++ script.js
@@ -6,65 +6,74 @@ const GBP_PRICE = 6.80
 const inputAmount = document.querySelector('#amount');
 const currency = document.querySelector('#currency');
 const form = document.querySelector('form')
-let valueFormatted
+const description = document.querySelector('#description')
+const result = document.querySelector('#result')
 
 inputAmount.focus()
 
 const footer = document.querySelector('footer')
 
 
-// Formatação do valor enquanto o usuário digitacoin
+// Formatação do valor enquanto o usuário digita
 inputAmount.addEventListener("input", (event) => {
   let value = inputAmount.value.replace(/\D/g, "");
 
-  inputAmount.value = value
-
+  // Formatação do valor para exibição em real 
   if (value.length >= 4) {
     value = value.replace(/(\d{2})$/, ",$1");
     value = value.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-
-    //atualiza valor no display
-    inputAmount.value = value;
   }
 
-  valueFormatted = value.replace(/[.,](?=[0-9]{3,}(?:[.,]|$))/g, '').replace(/[.,](?=[0-9]{2}$)/, '.')
+  //atualiza valor no display
+    inputAmount.value = value;
+
 })
+
+
+// Formatação do valor para exibição em real
+function valueFormatter(value) {
+  return Number(value).toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  })
+}
 
 
 form.onsubmit = (event) => {
   event.preventDefault()
+  // Formatação do valor para conversão
+  let value = inputAmount.value.replace(/[.,](?=[0-9]{3,}(?:[.,]|$))/g, '').replace(/[.,](?=[0-9]{2}$)/, '.')
 
   const currencyValue = currency.value
 
 
   switch (currencyValue) {
     case "USD": {
-      convertCurrency(valueFormatted, USD_PRICE, "US$")
+      convertCurrency(value, USD_PRICE, "US$")
       break
     }
     case "EUR": {
-      convertCurrency(valueFormatted, EUR_PRICE, "€")
+      convertCurrency(value, EUR_PRICE, "€")
       break
     }
     case "GBP": {
-      convertCurrency(valueFormatted, GBP_PRICE, "£")
+      convertCurrency(value, GBP_PRICE, "£")
       break
     }
   }
 }
 
+// Função de conversão de moedas
 function convertCurrency(amount, price, symbol) {
   let convertedValue
-  const description = document.querySelector('#description')
-  const result = document.querySelector('#result')
 
   try {
-    convertedValue = (amount * price).toFixed(2).replace('.', ',')
-    console.log(convertedValue)
-    convertedValue = convertedValue.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    convertedValue = (amount * price).toFixed(2)
+    convertedValue = valueFormatter(convertedValue)
 
-    description.textContent = `${symbol} ${inputAmount.value} = R$ ${convertedValue}`
-    result.textContent = `R$ ${convertedValue}`
+    description.textContent = `${symbol} ${inputAmount.value} = ${convertedValue}`
+    
+    result.textContent = `${convertedValue}`
 
     footer.classList.add('show-result')
   } catch (error) {
